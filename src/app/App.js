f01ce182cdc45e055a7248f8663b7a7915d66504
lmayegun <lmayegun@yahoo.com>
2020-06-10 18:41:19 +0100

    include react-router-dom and configure SwitchRoute and @history

--- src/app/App.js
+++ src/app/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
+import {Router} from 'react-router-dom';
+import history from '@history';
+
+import AppContext from './AppContext';
+import {SwitchRoute} from './route-system';
 import logo from './logo.svg';
 import '../styles/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContext.Provider>
+      <Router history={history}>
+        <SwitchRoute />
+      </Router>
+    </AppContext.Provider>
   );
 }
 
---
+++ src/app/route-system/SwitchRoute.js
@@ -0,0 +1,58 @@
+import React from "react";
+import {
+  Switch,
+  Route,
+} from "react-router-dom";
+
+export default function BasicRoute() {
+  return (
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+  );
+}
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
