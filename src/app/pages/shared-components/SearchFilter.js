26d4f3119f490587ac5b8f13f29d438eb7d7c114
lmayegun <lmayegun@yahoo.com>
2020-06-23 19:04:22 +0100

    wrap search filter components around form tags

--- src/app/pages/shared-components/SearchFilter.js
+++ src/app/pages/shared-components/SearchFilter.js
@@ -21,8 +21,109 @@ const initFormState = {
   category: "business"
 }
 
+const SearchFilter = ()=>{
+  const classes = styles();
+  const dispatch = useDispatch();
+  const {form, handleChange, setForm} = useForm(initFormState);
+
+  useEffect(()=>{
+    dispatch(NewsApiActions.setNewsApiContents({query:form.query, country:form.country, category:form.category}))
+    dispatch(FirebaseActions.getFirebaseContents({query:form.query, country:form.country, category:form.category}))
+  },[dispatch])
+
+  function handleSubmit(){
+    dispatch(NewsApiActions.setNewsApiContents({query:form.query, country:form.country, category:form.category}))
+  }
+
+  return(
+    <Paper className={classes.searchFilter} elevation={1}>
+      <form onSubmit={(e)=>{
+        e.preventDefault();
+        alert("Submitting form")
+      }}>
+        <div className={classes.search}>
+          <TextField
+            className={classes.textField}
+            id="outlined-basic"
+            label="Search"
+            variant="outlined"
+            name="query"
+            onChange={handleChange}
+            value={form.query}
+          />
+        </div>
+        <div className={classes.filters}>
+          <div className={"selector"}>
+            <FormControl className={classes.selector}>
+              <InputLabel id="demo-customized-select-label">Category</InputLabel>
+              <Select
+                labelId="demo-customized-select-label"
+                id="demo-customized-select"
+                value={form.category}
+                name={"category"}
+                onChange={handleChange}
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value={'business'}>Business</MenuItem>
+                <MenuItem value={'health'}>Health</MenuItem>
+                <MenuItem value={'sports'}>Sports</MenuItem>
+                <MenuItem value={'entertainment'}>Entertainment</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
+          <div className={"selector"}>
+            <FormControl className={classes.selector}>
+              <InputLabel id="demo-customized-select-label">Country</InputLabel>
+              <Select
+                labelId="demo-customized-select-label"
+                id="demo-customized-select"
+                value={form.country}
+                name="country"
+                onChange={handleChange}
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value={"us"}>United State</MenuItem>
+                <MenuItem value={"gb"}>United Kingdom</MenuItem>
+                <MenuItem value={"dm"}>Denmark</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
+        </div>
+        <div className={classes.submit}>
+          <Button
+             onClick={handleSubmit}
+             variant="contained"
+             color="primary"
+             size="small"
+             className={classes.button}
+             startIcon={<SendIcon />}
+           >
+             Submit
+           </Button>
+        </div>
+      </form>
+    </Paper>
+  );
+};
+
+export default SearchFilter;
+
 const styles = makeStyles( theme => (
   {
+    searchFilter:{
+      display:'flex',
+      flex: 1,
+      flexDirection: 'column',
+      alignItems: 'center',
+      padding: 10,
+      '& form':{
+        width: 100+'%',
+      }
+    },
     selector:{
       marginBottom: theme.spacing(2),
       width: 99+'%',
@@ -30,13 +131,6 @@ const styles = makeStyles( theme => (
     textField:{
       marginBottom: theme.spacing(2),
       width: 99+'%',
-    },
-    searchFilter:{
-      display:'flex',
-      flex: 1,
-      flexDirection: 'column',
-      alignItems: 'center',
-      padding: 10,
     },
     search:{
       display : 'flex',
@@ -58,89 +152,3 @@ const styles = makeStyles( theme => (
     }
   })
 );
-
-const SearchFilter = ()=>{
-  const classes = styles();
-  const dispatch = useDispatch();
-  const {form, handleChange, setForm} = useForm(initFormState);
-
-  useEffect(()=>{
-    dispatch(NewsApiActions.setNewsApiContents({query:form.query, country:form.country, category:form.category}))
-    dispatch(FirebaseActions.getFirebaseContents({query:form.query, country:form.country, category:form.category}))
-  },[dispatch])
-
-  function handleSubmit(){
-    dispatch(NewsApiActions.setNewsApiContents({query:form.query, country:form.country, category:form.category}))
-  }
-
-  return(
-    <Paper className={classes.searchFilter} elevation={1}>
-      <div className={classes.search}>
-        <TextField
-          className={classes.textField}
-          id="outlined-basic"
-          label="Search"
-          variant="outlined"
-          name="query"
-          onChange={handleChange}
-          value={form.query}
-        />
-      </div>
-      <div className={classes.filters}>
-        <div className={"selector"}>
-          <FormControl className={classes.selector}>
-            <InputLabel id="demo-customized-select-label">Category</InputLabel>
-            <Select
-              labelId="demo-customized-select-label"
-              id="demo-customized-select"
-              value={form.category}
-              name={"category"}
-              onChange={handleChange}
-            >
-              <MenuItem value="">
-                <em>None</em>
-              </MenuItem>
-              <MenuItem value={'business'}>Business</MenuItem>
-              <MenuItem value={'health'}>Health</MenuItem>
-              <MenuItem value={'sports'}>Sports</MenuItem>
-              <MenuItem value={'entertainment'}>Entertainment</MenuItem>
-            </Select>
-          </FormControl>
-        </div>
-        <div className={"selector"}>
-          <FormControl className={classes.selector}>
-            <InputLabel id="demo-customized-select-label">Country</InputLabel>
-            <Select
-              labelId="demo-customized-select-label"
-              id="demo-customized-select"
-              value={form.country}
-              name="country"
-              onChange={handleChange}
-            >
-              <MenuItem value="">
-                <em>None</em>
-              </MenuItem>
-              <MenuItem value={"us"}>United State</MenuItem>
-              <MenuItem value={"gb"}>United Kingdom</MenuItem>
-              <MenuItem value={"dm"}>Denmark</MenuItem>
-            </Select>
-          </FormControl>
-        </div>
-      </div>
-      <div className={classes.submit}>
-        <Button
-           onClick={handleSubmit}
-           variant="contained"
-           color="primary"
-           size="small"
-           className={classes.button}
-           startIcon={<SendIcon />}
-         >
-           Submit
-         </Button>
-       </div>
-    </Paper>
-  );
-};
-
-export default SearchFilter;
